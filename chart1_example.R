f9b94de7cc3510364309c92e86a935cbebb2d7ab
Jessica Wang <jessicaw.040131@gmail.com>
2023-08-03 20:42:36 -0700

    These are Jessica's changes

--- chart1_example.R
+++ chart1_example.R
@@ -1,7 +1,20 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+library(dplyr)
+library(tidyr)
 
+data <- read.csv("maryland_crash_report.csv")
+
+filtered_data <- data %>% 
+  select(Collision.Type)
+
+collision_types <- unique(filtered_data)
+collision_occurance <- table(data$Collision.Type)
+collision_occurance_df <- as.data.frame(collision_occurance)
+colnames(collision_occurance_df) <- c("Collision Type", "Count")
+
+collision_type_bar_graph <- ggplot(collision_counts_df, aes(x = `Collision Type`, y = Count)) +
+  geom_bar(stat = "identity", fill = "purple") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Total Occurance of Collision Types in Maryland from 2015-2023", x = "Collision Type", y = "Count")
+
+print(collision_type_bar_graph)
